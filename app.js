11af6cf157b84d5c0788f420d2deee42e5d97494
hamenyimana <hamenyimanavincent1@gmail.com>
2026-09-05 22:59:10 +0200

    modify app and gitignore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql2');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.json());
@@ -88,6 +88,6 @@ app.delete('/delete_students/:id', (req, res) => {
     });
 });     
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
 }); 
