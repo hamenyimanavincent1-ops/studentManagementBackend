9143d80dd592a57cb31ded91e54a9c5fd886a639
hamenyimana <hamenyimanavincent1@gmail.com>
2026-09-07 12:37:49 +0200

    add frontend url

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ require('dotenv').config();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://myapp.com",
-  credentials: true
+    origin: 'https://student-management-system-fronted.onrender.com/'
 }));
 
 
